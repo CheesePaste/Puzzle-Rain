bf650a34b188023fb8d2d878f5a0f6bae2805d78
hsz <hushizhih@gmail.com>
2025-11-15 13:21:59 +0800

    inital

---
+++ src/main/java/com/puzzle_rain/PuzzleRainDataGenerator.java
@@ -0,0 +1,11 @@
+package com.puzzle_rain;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class PuzzleRainDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
